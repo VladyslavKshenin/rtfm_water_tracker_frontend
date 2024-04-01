b07c5fdacc5e6004f3704864cd0bf3307496577c
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2024-04-01 13:59:50 +0300

    com1

--- src/components/pages/WelcomePage/WelcomePage.jsx
+++ src/components/pages/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,10 @@
 
 import { ReactComponent as Calendar} from 'images/benefits-1.svg'
 import { ReactComponent as Statistics} from 'images/benefits-2.svg'
-import { ReactComponent as PersonalRateSetting} from 'images/benefits-3.svg'
+import { ReactComponent as PersonalRateSetting } from 'images/benefits-3.svg'
+import styles from './WelcomePage.module.css'
+
+
 
 import { useLocation, useNavigate } from 'react-router-dom'
 
@@ -18,38 +21,38 @@ const WelcomePage = () => {
     <section>
       <div className="container">
         <div>
-          <h1>Water consumption tracker</h1>
-          <h2>Record daily water intake and track</h2>
-          <figure>
-            <figcaption>Tracker Benefits</figcaption>
-            <ul>
-              <li>
+          <h1 className={styles.title}>Water consumption tracker</h1>
+          <h2 className={styles.subtitle}>Record daily water intake and track</h2>
+          <figure className={styles.trackerCont}>
+            <figcaption className={styles.benefits}>Tracker Benefits</figcaption>
+            <ul className={styles.listBenefits}>
+              <li className={styles.itemBenefits}>
                 <Calendar width={32} height={32}/>
-                <p>Habit drive</p>
+                <p className={styles.description}>Habit drive</p>
               </li>
-              <li>
+              <li className={styles.itemBenefits}>
                 <Statistics width={32} height={32}/>
-                <p>View statistics</p>
+                <p className={styles.description}>View statistics</p>
               </li>
-              <li>
+              <li className={styles.itemBenefits}>
                 <PersonalRateSetting width={32} height={32}/>
-                <p>Personal rate setting</p>
+                <p className={styles.description}>Personal rate setting</p>
               </li>
             </ul>
           </figure>
-          <button type="button" onClick={handleravigate}>Try tracker</button>
+          <button className={styles.tryTrackerBtn} type="button" onClick={handleravigate}>Try tracker</button>
         </div>
         <div>
           <figure>
-            <figcaption>Why drink water</figcaption>
-            <ul>
-              <li>Supply of nutrients to all organs  </li>
-              <li>Providing oxygen to the lungs</li>
-              <li>Maintaining the work of the heart</li>
-              <li>Release of processed substances</li>
-              <li>Ensuring the stability of the internal environment</li>
-              <li>Maintaining within the normal temperature</li>
-              <li>Maintaining an immune system capable of resisting disease</li>
+            <figcaption className={styles.whyDrink}>Why drink water</figcaption>
+            <ul className={styles.listReasons}>
+              <li className={styles.itemReasons}>Supply of nutrients to all organs  </li>
+              <li className={styles.itemReasons}>Providing oxygen to the lungs</li>
+              <li className={styles.itemReasons}>Maintaining the work of the heart</li>
+              <li className={styles.itemReasons}>Release of processed substances</li>
+              <li className={styles.itemReasons}>Ensuring the stability of the internal environment</li>
+              <li className={styles.itemReasons}>Maintaining within the normal temperature</li>
+              <li className={styles.itemReasons}>Maintaining an immune system capable of resisting disease</li>
             </ul>
           </figure>
         </div>

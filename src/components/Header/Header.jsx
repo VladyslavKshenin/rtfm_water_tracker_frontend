7fb9fe244babea646124bbc2029f8a63b7a55e0d
Vladyslav <134953305+VladyslavKshenin@users.noreply.github.com>
2024-04-02 19:57:28 +0300

    add hover to SignIn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,26 @@
-import { Svg } from 'components/Icons/Icons'
+import { Svg } from 'components/Icons/Icons';
 import Icons from '../../images/icons.svg';
-import { Link } from 'react-router-dom'
-import css from "./Header.module.css"
+import { Link } from 'react-router-dom';
+import css from './Header.module.css';
 
 export const Header = () => {
-
-  //Для спан-юзера зроби бордер 
+  //Для спан-юзера зроби бордер
   // <span>
-  // <Svg id="#user" width={14} height={18}/>    
+  // <Svg id="#user" width={14} height={18}/>
   // </span>
   return (
     <header>
-      <div className='container'>
+      <div className="container">
         <nav className={css.nav}>
           <ul className={css.list}>
             <li>
               <Link to="/welcome">
-                <Svg id="#logo" width={102} height={48}/> 
+                <Svg id="#logo" width={102} height={48} />
               </Link>
             </li>
             <li>
               <Link to="/signin" className={css.link}>
-                <span>Sign In</span> 
+                <span className={css.signin}>Sign In</span>
                 <svg className={css.icon} width="28" height="28">
                   <use href={Icons + '#user-1'}></use>
                 </svg>
@@ -31,5 +30,5 @@ export const Header = () => {
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};

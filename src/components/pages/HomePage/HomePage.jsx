528ac0e991ba8c9a9efdb7d9bc0fef1dd2bbfd6b
Vladyslavi <135136418+Vladyslavi@users.noreply.github.com>
2024-04-03 12:04:40 +0300

    update HomePage

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -2,8 +2,15 @@ import { DailyNorma } from 'components/DailyNorma/DailyNorma';
 import Modal from 'components/Modal/Modal';
 import UserLogoutModal from 'components/UserLogoutModal/UserLogoutModal';
 import { WaterProgress } from 'components/WaterProgress/WaterProgress';
+import { useSelector } from 'react-redux';
+import Loader from 'components/Loader/Loader';
+
 
 const HomePage = () => {
+
+  const isLoading = useSelector();
+
+
   return (
     <>
       <Modal>
@@ -11,6 +18,7 @@ const HomePage = () => {
       </Modal>
       <DailyNorma />
       <WaterProgress />
+      {isLoading && <Loader />}
     </>
   );
 };

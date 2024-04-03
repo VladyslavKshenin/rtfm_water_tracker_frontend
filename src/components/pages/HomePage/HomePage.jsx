3e0a326ad7f07b4aceec683a2fc00b7a1bd01f0e
Vladyslavi <135136418+Vladyslavi@users.noreply.github.com>
2024-04-03 18:30:40 +0300

    Udpdate Homepage

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -2,13 +2,14 @@ import { DailyNorma } from 'components/DailyNorma/DailyNorma';
 import Modal from 'components/Modal/Modal';
 import UserLogoutModal from 'components/UserLogoutModal/UserLogoutModal';
 import { WaterProgress } from 'components/WaterProgress/WaterProgress';
+import { selectIsLoggedIn } from 'store/auth/authSelectors';
 import { useSelector } from 'react-redux';
 import Loader from 'components/Loader/Loader';
 
 
 const HomePage = () => {
 
-  const isLoading = useSelector();
+  const isLoading = useSelector(selectIsLoggedIn);
 
 
   return (

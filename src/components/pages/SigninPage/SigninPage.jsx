4d401a75a95a35ad05b350573adde58776909cd2
Vladyslav <134953305+VladyslavKshenin@users.noreply.github.com>
2024-04-03 12:45:57 +0300

    add background-image for register and login page

--- src/components/pages/SigninPage/SigninPage.jsx
+++ src/components/pages/SigninPage/SigninPage.jsx
@@ -1,17 +1,16 @@
 // import AuthForm from 'components/AuthForm/AuthForm'
-import { useDispatch ,useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logIn } from '../../../store/auth/authOperations';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { useState } from 'react';
 
 import { selectError } from 'store/auth/authSelectors';
 
 const SigninPage = () => {
-
   const dispatch = useDispatch();
-  const [logPassword,setLogPassword]=useState('');
-  const [logEmail,setLogEmail]=useState('');
+  const [logPassword, setLogPassword] = useState('');
+  const [logEmail, setLogEmail] = useState('');
   const error = useSelector(selectError);
   const navigate = useNavigate();
   console.log(error);
@@ -24,49 +23,60 @@ const SigninPage = () => {
         password: Form.elements.password.value,
       })
     );
-    if(error===null){
-    setLogEmail('')
-    setLogPassword('')
-    navigate('/home')
+    if (error === null) {
+      setLogEmail('');
+      setLogPassword('');
+      navigate('/home');
     }
   };
-  const handleChange = (e)=>{
-    const {name,value}=e.currentTarget;
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
 
-    if(name==='email'){
-    setLogEmail(value)
-  }
-    else {
-      setLogPassword(value)
+    if (name === 'email') {
+      setLogEmail(value);
+    } else {
+      setLogPassword(value);
     }
-  }
+  };
 
   return (
     <>
-      <h1>Log In</h1>
+      <section className="signSection">
+        <div className="signMain">
+          <h1>Log In</h1>
 
-      <div>
-        <form onSubmit={handleSubmit} autoComplete="off">
-          <label>
-            E-mail:
-            <input type="email" name="email" onChange={handleChange} value={logEmail}/>
-          </label>
-          <label>
-            Password:
-            <input type="password" name="password" onChange={handleChange} value={logPassword} />
-          </label>
-          <button type="submit">
-            Sign In
-          </button>
-        </form>
+          <div>
+            <form onSubmit={handleSubmit} autoComplete="off">
+              <label>
+                E-mail:
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={logEmail}
+                />
+              </label>
+              <label>
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  value={logPassword}
+                />
+              </label>
+              <button type="submit">Sign In</button>
+            </form>
 
-        <p>
-          <Link to="/signup">Sign Up</Link>
-        </p>
-      </div>
+            <p>
+              <Link to="/signup">Sign Up</Link>
+            </p>
+          </div>
+        </div>
+      </section>
     </>
     // <AuthForm/>
-  )
-}
+  );
+};
 
-export default SigninPage
+export default SigninPage;
--- src/components/pages/SignupPage/SignupPage.jsx
+++ src/components/pages/SignupPage/SignupPage.jsx
@@ -1,26 +1,26 @@
 // import AuthForm from 'components/AuthForm/AuthForm'
-import { Link ,useNavigate } from "react-router-dom"
-import { selectError } from "store/auth/authSelectors.js";
+import { Link, useNavigate } from 'react-router-dom';
+import { selectError } from 'store/auth/authSelectors.js';
 import { useDispatch, useSelector } from 'react-redux';
 import { register } from '../../../store/auth/authOperations';
 import { useState } from 'react';
 
 const SignupPage = () => {
   const dispatch = useDispatch();
-  const error = useSelector(selectError)
-  console.log('error', error)
-  const [regEmail,setRegEmail]=useState('');
+  const error = useSelector(selectError);
+  console.log('error', error);
+  const [regEmail, setRegEmail] = useState('');
 
-  const [regPassword,setRegPassword]=useState('');
-  const [regRepeatPassword,setRegRepeatPassword]=useState('');
+  const [regPassword, setRegPassword] = useState('');
+  const [regRepeatPassword, setRegRepeatPassword] = useState('');
   const navigate = useNavigate();
   const handleSubmit = e => {
     e.preventDefault();
     const Form = e.currentTarget;
 
-    console.log(Form.elements)
-    if(regPassword!==regRepeatPassword){
-      alert('Паролі не співпадають')
+    console.log(Form.elements);
+    if (regPassword !== regRepeatPassword) {
+      alert('Паролі не співпадають');
       return;
     }
     dispatch(
@@ -29,56 +29,71 @@ const SignupPage = () => {
         password: Form.elements.password.value,
       })
     );
-    
-    if( error===null){
-      setRegEmail('')
-      setRegPassword('')
-      setRegRepeatPassword('')
-      navigate('/signin')
+
+    if (error === null) {
+      setRegEmail('');
+      setRegPassword('');
+      setRegRepeatPassword('');
+      navigate('/signin');
     }
   };
-  const handleChange = (e)=>{
-    const {name,value}=e.currentTarget;
-     if(name==='email'){
-      setRegEmail(value)
-     }
-    else if(name==='password') {
-      setRegPassword(value)
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+    if (name === 'email') {
+      setRegEmail(value);
+    } else if (name === 'password') {
+      setRegPassword(value);
+    } else {
+      setRegRepeatPassword(value);
     }
-    else {
-      setRegRepeatPassword(value)
-    }
-  }
+  };
 
   return (
     // <AuthForm/>
     <>
-    <h1>Sign Up</h1>
-    <div>
-  <form onSubmit={handleSubmit} autoComplete="off">
-    <label>
-    Enter your email
-      <input type="email" name="email" onChange={handleChange} value={regEmail}/>
-    </label>
-     <label>
-    Enter your password
-      <input type="password" name="password" onChange={handleChange} value={regPassword}/>
-    </label> 
-    <label>
-    Repeat password
-      <input type="password" name="password-repeat" onChange={handleChange} value={regRepeatPassword}  />
-    </label>
-    <button type="submit">
-      Sign Up
-    </button>
-  </form>
+      <section className="signSection">
+        <div className="signMain">
+          <h1>Sign Up</h1>
+          <div>
+            <form onSubmit={handleSubmit} autoComplete="off">
+              <label>
+                Enter your email
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={regEmail}
+                />
+              </label>
+              <label>
+                Enter your password
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  value={regPassword}
+                />
+              </label>
+              <label>
+                Repeat password
+                <input
+                  type="password"
+                  name="password-repeat"
+                  onChange={handleChange}
+                  value={regRepeatPassword}
+                />
+              </label>
+              <button type="submit">Sign Up</button>
+            </form>
 
-  <p>
-    <Link to="/signin">Log In</Link>
-  </p>
-    </div>
+            <p>
+              <Link to="/signin">Log In</Link>
+            </p>
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default SignupPage
+export default SignupPage;

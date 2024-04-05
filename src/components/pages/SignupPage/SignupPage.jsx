ce78ed37cb11307df7253c315d8ea5dfc60f35a0
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2024-04-05 15:07:22 +0300

    add

--- src/components/pages/SignupPage/SignupPage.jsx
+++ src/components/pages/SignupPage/SignupPage.jsx
@@ -1,9 +1,9 @@
 // import AuthForm from 'components/AuthForm/AuthForm'
 import { Link, useNavigate } from 'react-router-dom';
-import css from '../SigninPage/SigninPage.module.css'
+import css from '../SigninPage/SigninPage.module.css';
 import { useDispatch } from 'react-redux';
 import { register } from 'store/auth/authOperations';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { ReactComponent as Eye } from 'images/eye.svg';
 import { ReactComponent as EyeSlash } from 'images/eye-slash.svg';
 
@@ -11,7 +11,6 @@ const SignupPage = () => {
   const dispatch = useDispatch();
   const [regEmail, setRegEmail] = useState('');
   const [flagWatch, setFlagWatch] = useState(false);
-  const [flagSignup,setFlagSignup] = useState(false);
 
   const [regPassword, setRegPassword] = useState('');
   const [regRepeatPassword, setRegRepeatPassword] = useState('');
@@ -28,7 +27,12 @@ const SignupPage = () => {
       alert('Введіть коректний email');
       return;
     }
-    if(regPassword.length < 8 || regRepeatPassword.length<8 || regPassword.length > 64 || regRepeatPassword.length > 64){
+    if (
+      regPassword.length < 8 ||
+      regRepeatPassword.length < 8 ||
+      regPassword.length > 64 ||
+      regRepeatPassword.length > 64
+    ) {
       alert('Пароль повинен містити від 8 до 64 символів!');
       return;
     }
@@ -49,10 +53,11 @@ const SignupPage = () => {
         setRegEmail('');
         setRegPassword('');
         setRegRepeatPassword('');
-        navigate("/signin");
-       
+        navigate('/signin');
       })
-      .catch((error) =>  {console.log(error);});
+      .catch(error => {
+        alert('Такий email вже використовується');
+      });
   };
 
   const handleChange = e => {
@@ -67,54 +72,78 @@ const SignupPage = () => {
   };
 
   return (
-    
     <section className="signSection">
       <div className="signMain">
         <div className="container">
           <div className={css.contPosition}>
-            <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+            <form
+              className={css.form}
+              onSubmit={handleSubmit}
+              autoComplete="off"
+            >
               <h1 className={css.title}>Sign Up</h1>
               <p className={css.description}>Enter your email</p>
-    <label>
-      <input placeholder="E-mail" className={css.input} type="email" name="email" onChange={handleChange} value={regEmail}/>
-    </label>
-    <p className={css.description}>Enter your password</p>
-     <label  className={css.form_label}>
-      <input placeholder="Password" className={css.input} type={flagWatch ? 'text' : 'password'} name="password" onChange={handleChange} value={regPassword}/>
-      <div
-              onClick={() => {
-                setFlagWatch(!flagWatch);
-              }}
-              className={css.svg_input_password}
-            >
-              {flagWatch ? <Eye /> : <EyeSlash />}
-            </div>
-    </label>
-    <p className={css.description}>Repeat password</p> 
-    <label  className={css.form_label}>
-      <input placeholder="Repeat password" className={css.input}  type={flagWatch ? 'text' : 'password'} name="password-repeat" onChange={handleChange} value={regRepeatPassword}  />
-      <div
-              onClick={() => {
-                setFlagWatch(!flagWatch);
-              }}
-              className={css.svg_input_password}
-            >
-              {flagWatch ? <Eye /> : <EyeSlash />}
-            </div>
-    </label>
-    <button className={css.button} type="submit">
-      Sign Up
+              <label>
+                <input
+                  placeholder="E-mail"
+                  className={css.input}
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={regEmail}
+                />
+              </label>
+              <p className={css.description}>Enter your password</p>
+              <label className={css.form_label}>
+                <input
+                  placeholder="Password"
+                  className={css.input}
+                  type={flagWatch ? 'text' : 'password'}
+                  name="password"
+                  onChange={handleChange}
+                  value={regPassword}
+                />
+                <div
+                  onClick={() => {
+                    setFlagWatch(!flagWatch);
+                  }}
+                  className={css.svg_input_password}
+                >
+                  {flagWatch ? <Eye /> : <EyeSlash />}
+                </div>
+              </label>
+              <p className={css.description}>Repeat password</p>
+              <label className={css.form_label}>
+                <input
+                  placeholder="Repeat password"
+                  className={css.input}
+                  type={flagWatch ? 'text' : 'password'}
+                  name="password-repeat"
+                  onChange={handleChange}
+                  value={regRepeatPassword}
+                />
+                <div
+                  onClick={() => {
+                    setFlagWatch(!flagWatch);
+                  }}
+                  className={css.svg_input_password}
+                >
+                  {flagWatch ? <Eye /> : <EyeSlash />}
+                </div>
+              </label>
+              <button className={css.button} type="submit">
+                Sign Up
               </button>
               <p className={css.sign}>
-    <Link to="/signin">Sign In</Link>
-  </p>
-  </form>
-    </div>
-      </div>
+                <Link to="/signin">Sign In</Link>
+              </p>
+            </form>
+          </div>
         </div>
+      </div>
     </section>
     // <AuthForm/>
-  )
-}
+  );
+};
 
 export default SignupPage;

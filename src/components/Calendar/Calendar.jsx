ed50ac69213adb6ac8c4727a7e33d7d6d015f495
Vladyslav <134953305+VladyslavKshenin@users.noreply.github.com>
2024-04-06 00:05:37 +0300

    add style

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -32,30 +32,34 @@ const CalendarContainer = () => {
   const backendData = Array(daysOfMonth.length).fill(0);
 
   return (
-    <div className={css.calendarContainer}>
-      <div className={css.calendarHeader}>
-        <h1 className={css.titleMonth}>Month</h1>
-        <div className={css.monthNameContainer}>
-          <button className={css.buttonMonth} onClick={handlePrevMonth}>
-            &lt;
-          </button>
-          <p className={css.monthName}>{format(currentDate, 'MMMM yyyy')}</p>
-          <button className={css.buttonMonth} onClick={handleNextMonth}>
-            &gt;
-          </button>
-        </div>
-      </div>
-      <div className={css.calendarGrid}>
-        {daysOfMonth.map((day, index) => (
-          <div className={css.day} key={index}>
-            <div
-              className={`${css.calendarDay} ${isToday(day) ? css.today : ''}`}
-            >
-              {format(day, 'd')}
-            </div>
-            <div>{backendData[index]}%</div>
+    <div className={css.calendarSection}>
+      <div className={css.calendarContainer}>
+        <div className={css.calendarHeader}>
+          <h1 className={css.titleMonth}>Month</h1>
+          <div className={css.monthNameContainer}>
+            <button className={css.buttonMonth} onClick={handlePrevMonth}>
+              &lt;
+            </button>
+            <p className={css.monthName}>{format(currentDate, 'MMMM yyyy')}</p>
+            <button className={css.buttonMonth} onClick={handleNextMonth}>
+              &gt;
+            </button>
           </div>
-        ))}
+        </div>
+        <div className={css.calendarGrid}>
+          {daysOfMonth.map((day, index) => (
+            <div className={css.day} key={index}>
+              <div
+                className={`${css.calendarDay} ${
+                  isToday(day) ? css.today : ''
+                }`}
+              >
+                {format(day, 'd')}
+              </div>
+              <div>{backendData[index]}%</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );

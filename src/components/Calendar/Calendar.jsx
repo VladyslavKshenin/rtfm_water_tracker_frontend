e4b69763fd953efd51aeec2bb305ee7ec5dc5308
Vladyslav <134953305+VladyslavKshenin@users.noreply.github.com>
2024-04-06 17:27:22 +0300

    apdate popover to calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -10,8 +10,29 @@ import {
 } from 'date-fns';
 import css from './Calendar.module.css';
 
+// Компонент поповеру
+const Popover = ({ date, dailyNorma, fulfillment, waterServings }) => {
+  const formattedDate = format(date, 'd MMMM');
+  return (
+    <div className={css.popover}>
+      <p className={css.popoverDate}>{formattedDate}</p>
+      <p className={css.popoverList}>
+        Daily norma: <span className={css.popoverDate}>{dailyNorma}L</span>
+      </p>
+      <p className={css.popoverList}>
+        Fulfillment of the daily norm:{' '}
+        <span className={css.popoverDate}>{fulfillment}%</span>
+      </p>
+      <p className={css.popoverList}>
+        How many servings of water:{' '}
+        <span className={css.popoverDate}>{waterServings}</span>
+      </p>
+    </div>
+  );
+};
 const CalendarContainer = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [popoverData, setPopoverData] = useState(null);
 
   const handleNextMonth = () => {
     setCurrentDate(addMonths(currentDate, 1));
@@ -29,7 +50,11 @@ const CalendarContainer = () => {
   });
 
   // Поки використаємо статичне значення 0 для кожної дати
-  const backendData = Array(daysOfMonth.length).fill(0);
+  const backendData = Array(daysOfMonth.length).fill({
+    dailyNorma: 1.5,
+    fulfillment: 0,
+    waterServings: 6,
+  });
 
   return (
     <div className={css.calendarContainer}>
@@ -50,12 +75,17 @@ const CalendarContainer = () => {
           <div className={css.day} key={index}>
             <div
               className={`${css.calendarDay} ${isToday(day) ? css.today : ''}`}
+              onMouseEnter={() =>
+                setPopoverData({ ...backendData[index], date: day })
+              }
+              onMouseLeave={() => setPopoverData(null)}
             >
               {format(day, 'd')}
             </div>
-            <div>{backendData[index]}%</div>
+            <div>{backendData[index].fulfillment}%</div>
           </div>
         ))}
+        {popoverData && <Popover {...popoverData} date={popoverData.date} />}
       </div>
     </div>
   );

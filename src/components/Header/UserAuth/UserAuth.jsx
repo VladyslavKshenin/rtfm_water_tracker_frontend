a9ec2bb9688cd281bb0e87c2d7e40b0665ab0a63
Vladyslav <134953305+VladyslavKshenin@users.noreply.github.com>
2024-04-07 15:51:28 +0300

    Update UserAuth.jsx

--- src/components/Header/UserAuth/UserAuth.jsx
+++ src/components/Header/UserAuth/UserAuth.jsx
@@ -1,34 +1,35 @@
-import { Link } from "react-router-dom";
-import { selectIsLoggedIn, selectUser } from 'store/auth/authSelectors';
+import { Link } from 'react-router-dom';
+import { selectIsLoggedIn } from 'store/auth/authSelectors';
 import { useSelector } from 'react-redux';
 
 import Icons from '../../../images/icons.svg';
-import css from "../Header.module.css";
-import BasicPopover from "../../BasicPopover/BasicPopover";
-
+import css from '../Header.module.css';
+import BasicPopover from '../../BasicPopover/BasicPopover';
 
 const UserAuth = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  // const usersInformation = useSelector(selectUser)
+  // const {name, email, avatar} = usersInformation;
+  // console.log('first', usersInformation)
 
-    const isLoggedIn = useSelector(selectIsLoggedIn) 
-    const usersInformation = useSelector(selectUser)
-    const {name, email, avatar} = usersInformation;
-    console.log('first', usersInformation)
-
-    return (
-        <>
-            {!isLoggedIn  ? <Link to="/signin" className={css.link}>
-                                <span className={css.signin}>Sign In</span>
-                                <svg className={css.icon} width="28" height="28">
-                                    <use href={Icons + '#user-1'}></use>
-                                </svg>
-                            </Link>
-                          : <span>
-                                <span>xzcwsacxsacsc</span>
-                                <img src="" alt="" />
-                                <BasicPopover/>
-                            </span>}
+  return (
+    <>
+      {!isLoggedIn ? (
+        <Link to="/signin" className={css.link}>
+          <span className={css.signin}>Sign In</span>
+          <svg className={css.icon} width="28" height="28">
+            <use href={Icons + '#user-1'}></use>
+          </svg>
+        </Link>
+      ) : (
+        <span>
+          <span>xzcwsacxsacsc</span>
+          <img src="" alt="" />
+          <BasicPopover />
+        </span>
+      )}
     </>
-    )
-}
+  );
+};
 
-export default UserAuth
+export default UserAuth;

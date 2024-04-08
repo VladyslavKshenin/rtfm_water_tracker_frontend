5a4fe2cd18785b806f0ac7e6cb2adb278f60a50e
Vladyslav <134953305+VladyslavKshenin@users.noreply.github.com>
2024-04-08 13:01:26 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,24 @@
-import { Suspense, lazy, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import { useDispatch } from "react-redux"
+import { Suspense, lazy, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import Loader from "./Loader/Loader";
-import { SharedLayout } from "./SharedLayout/SharedLayout";
-import WelcomePage from "./pages/WelcomePage/WelcomePage";
-import PublicRoute from "./guards/PublicRoute";
-import PrivateRoute from "./guards/PrivateRoute";
-import { currentUser } from "store/auth/authOperations";
+import Loader from './Loader/Loader';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import WelcomePage from './pages/WelcomePage/WelcomePage';
+import PublicRoute from './guards/PublicRoute';
+import PrivateRoute from './guards/PrivateRoute';
+import { currentUser } from 'store/auth/authOperations';
 
-const SigninPage = lazy(() => import('./pages/SigninPage/SigninPage'))
-const SignupPage = lazy(() => import('./pages/SignupPage/SignupPage'))
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'))
+const SigninPage = lazy(() => import('./pages/SigninPage/SigninPage'));
+const SignupPage = lazy(() => import('./pages/SignupPage/SignupPage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 
 export const App = () => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-
-   dispatch(currentUser())
-    
-  }, [dispatch])
-
+    dispatch(currentUser());
+  }, [dispatch]);
 
   return (
     <>
@@ -30,27 +26,39 @@ export const App = () => {
       <Suspense fallback={<>Loading...</>}>
         <Routes>
           <Route path="/" element={<SharedLayout />}>
-            <Route index element ={<WelcomePage/>}/>
-            <Route path="welcome" element={
-              <PublicRoute>
-                <WelcomePage />
-              </PublicRoute>
-            } />
-            <Route path="signin" element={
-              <PublicRoute>
-                <SigninPage />
-              </PublicRoute>
-            } />
-            <Route path="signup" element={
-              <PublicRoute>
-                <SignupPage />
-              </PublicRoute>
-            } />
-            <Route path="home" element={
-              // <PrivateRoute>
-                <HomePage />
-              // </PrivateRoute>
-            } />
+            <Route index element={<WelcomePage />} />
+            <Route
+              path="welcome"
+              element={
+                <PublicRoute>
+                  <WelcomePage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="signin"
+              element={
+                <PublicRoute>
+                  <SigninPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="signup"
+              element={
+                <PublicRoute>
+                  <SignupPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="home"
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            />
           </Route>
           <Route path="*" element={<div>404</div>} />
         </Routes>
